bd6ec4c62a59cfb4b4b58c9c21b0ca3d2819ea8b
azaqis <55734899+azaqis@users.noreply.github.com>
2021-02-05 11:22:54 +0100

    hamburgerMenu fungerar
    .. men går ej skifta mellan intents via menyn

--- app/src/main/java/se/anad19ps/student/turtle/BluetoothActivity.kt
+++ app/src/main/java/se/anad19ps/student/turtle/BluetoothActivity.kt
@@ -1,29 +1,16 @@
 package se.anad19ps.student.turtle
 
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.transition.Slide
-import kotlinx.android.synthetic.main.activity_bluetooth.*
 import kotlinx.android.synthetic.main.drawer_layout.*
 import kotlinx.android.synthetic.main.top_bar.*
 
 
 class BluetoothActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        Log.d("BLUETOOTH", "1")
         setContentView(R.layout.activity_bluetooth)
-        Log.d("BLUETOOTH", "2")
-        HamburgerMenu(navView)
-        Log.d("BLUETOOTH", "3")
 
+        HamburgerMenu().setUpHamburgerMenu(this, navView, drawerLayout, hamburgerMenuIcon)
     }
-
 }
--- app/src/main/java/se/anad19ps/student/turtle/HamburgerMenu.kt
+++ app/src/main/java/se/anad19ps/student/turtle/HamburgerMenu.kt
@@ -1,66 +1,67 @@
 package se.anad19ps.student.turtle
 
-import android.util.Log
+import android.content.Intent
+import android.app.Activity
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.drawer_layout.*
-import kotlinx.android.synthetic.main.top_bar.*
 import android.content.Context
-import java.security.AccessController.getContext
+import android.widget.ImageView
+import androidx.drawerlayout.widget.DrawerLayout
 
-
-class HamburgerMenu(navView: NavigationView) : AppCompatActivity(){
+class HamburgerMenu() : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
-    init {
-        Log.d("HAMBURGER", "1")
+
+    fun setUpHamburgerMenu(
+        con: Context,
+        navView: NavigationView,
+        drawerLayout: DrawerLayout,
+        hamburgerMenuIcon: ImageView
+    ) {
+
         toggle = ActionBarDrawerToggle(
-            this,
-            findViewById(R.id.drawerLayout),
+            con as Activity?,
+            drawerLayout,
             R.string.open,
             R.string.close
         )
-        Log.d("HAMBURGER", "2")
+
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setIcon(R.drawable.hamburger_menu_icon)
-        Log.d("HAMBURGER", "3")
         hamburgerMenuIcon.setOnClickListener {
-            Log.d("najs", "asdasd")
             drawerLayout.openDrawer(GravityCompat.START)
         }
-        Log.d("HAMBURGER", "4")
-
         navView.setNavigationItemSelectedListener {
+            if (it.itemId == R.id.item1){
+                startActivity(Intent(con, BluetoothActivity::class.java))
+            }
+
             when (it.itemId) {
                 R.id.item1 -> Toast.makeText(
-                    applicationContext,
+                    con,
                     "Clicked item 1",
                     Toast.LENGTH_SHORT
                 ).show()
                 R.id.item2 -> Toast.makeText(
-                    applicationContext,
+                    con,
                     "Clicked item 2",
                     Toast.LENGTH_SHORT
                 ).show()
                 R.id.item3 -> Toast.makeText(
-                    applicationContext,
+                    con,
                     "Clicked item 3",
                     Toast.LENGTH_SHORT
                 ).show()
             }
             true
         }
-        Log.d("najs", "asdasd")
-        drawerLayout.openDrawer(GravityCompat.START)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)){
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
 
