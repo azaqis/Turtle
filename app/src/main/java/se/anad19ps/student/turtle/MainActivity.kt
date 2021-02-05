ba708aa87dbf4998e5c3bcfc9aef2653a877f6d8
j.appelberg <apjo19kn@student.ju.se>
2021-02-05 11:21:05 +0100

    Spinner för controllerView
    Spinner funktionell men ej centrerad

--- app/src/main/java/se/anad19ps/student/turtle/MainActivity.kt
+++ app/src/main/java/se/anad19ps/student/turtle/MainActivity.kt
@@ -12,62 +12,13 @@ import kotlinx.android.synthetic.main.drawer_layout.*
 import kotlinx.android.synthetic.main.top_bar.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        toggle = ActionBarDrawerToggle(
-            this,
-            findViewById(R.id.drawerLayout),
-            R.string.open,
-            R.string.close
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setIcon(R.drawable.hamburger_menu_icon)
-
-        hamburgerMenuIcon.setOnClickListener {
-            Log.d("najs", "asdasd")
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
-
-        navView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.item1 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked item 1",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.item2 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked item 2",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.item3 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked item 3",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            true
-        }
-        Log.d("MAIN", "1")
-        drawerLayout.openDrawer(GravityCompat.START)
-        Log.d("MAIN", "2")
-        startActivity(Intent(this, BluetoothActivity::class.java))
-        Log.d("MAIN", "3")
+        val intent = Intent(this, ControllerActivity::class.java)
+        startActivity(intent)
     }
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            if(toggle.onOptionsItemSelected(item)){
-                return true
-            }
-
-            return super.onOptionsItemSelected(item)
-        }
-
-
 }
