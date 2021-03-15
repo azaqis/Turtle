d9d0d49753e59e9c8cedb083c4cb7093d15af016
Johnbombadill <62593587+Johnbombadill@users.noreply.github.com>
2021-03-15 11:52:48 +0100

    Tog bort en import

--- app/src/main/java/se/anad19ps/student/turtle/ProgrammingActivity.kt
+++ app/src/main/java/se/anad19ps/student/turtle/ProgrammingActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
