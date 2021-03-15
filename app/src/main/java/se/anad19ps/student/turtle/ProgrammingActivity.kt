328b88b447dc1cc445a7fd6bd09ba42594a44084
azaqis <55734899+azaqis@users.noreply.github.com>
2021-03-15 11:47:10 +0100

    back button

--- app/src/main/java/se/anad19ps/student/turtle/ProgrammingActivity.kt
+++ app/src/main/java/se/anad19ps/student/turtle/ProgrammingActivity.kt
@@ -266,6 +266,7 @@ class ProgrammingActivity : AppCompatActivity(), ProgrammingRecyclerAdapter.Item
                 displayDialogAskIfWantToSave(newIntent)
             } else {
                 startActivity(newIntent)
+                finish()
             }
 
         }
@@ -839,6 +840,7 @@ class ProgrammingActivity : AppCompatActivity(), ProgrammingRecyclerAdapter.Item
                 DialogInterface.BUTTON_NEGATIVE -> {
                     Utils.UtilsObject.showUpdatedToast(getString(R.string.project_not_saved), this)
                     startActivity(intent)
+                    finish()
                 }
                 DialogInterface.BUTTON_POSITIVE -> {
                     displayDialogInputName(intent)
