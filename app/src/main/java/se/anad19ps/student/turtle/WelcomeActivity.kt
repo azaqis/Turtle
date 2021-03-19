53d96aa98d2f89e451b6318170ad7fc4f208dc6a
azaqis <55734899+azaqis@users.noreply.github.com>
2021-03-19 15:49:33 +0100

    Introduction Done

--- app/src/main/java/se/anad19ps/student/turtle/WelcomeActivity.kt
+++ app/src/main/java/se/anad19ps/student/turtle/WelcomeActivity.kt
@@ -1,18 +1,49 @@
 package se.anad19ps.student.turtle
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Button
 import androidx.annotation.NonNull
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private enum class IntroductionState{
+        INTRODUCTION_WELCOME,
+        INTRODUCTION_BLUETOOTH,
+        INTRODUCTION_PROGRAMMING,
+        INTRODUCTION_REMOTE_CONTROLLER,
+        INTRODUCTION_CUSTOM_COMMANDS
+    }
+
+    private var introductionState = IntroductionState.INTRODUCTION_WELCOME
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        if(!isFirstTimeOpeningApp()){
+            exitIntroduction()
+        }
+
+        if(savedInstanceState != null){
+            introductionState = IntroductionState.valueOf(savedInstanceState.getString("INTRODUCTION_STATE")!!)
+            when(introductionState){
+                IntroductionState.INTRODUCTION_WELCOME ->{}
+                IntroductionState.INTRODUCTION_BLUETOOTH -> showIntroductionBluetooth()
+                IntroductionState.INTRODUCTION_PROGRAMMING -> showIntroductionProgramming()
+                IntroductionState.INTRODUCTION_REMOTE_CONTROLLER -> showIntroductionRemoteController()
+                IntroductionState.INTRODUCTION_CUSTOM_COMMANDS -> showIntroductionCustomCommands()
+            }
+        }
         setupButtons()
     }
 
@@ -30,47 +61,93 @@ class WelcomeActivity : AppCompatActivity() {
         val btnFinish = findViewById<View>(R.id.welcome_button_finish) as Button
 
         btnNext1.setOnClickListener{
-            welcome_linear_layout_intro.visibility = View.GONE
-            welcome_linear_layout_bluetooth.visibility = View.VISIBLE
+            showIntroductionBluetooth()
         }
         btnNext2.setOnClickListener{
-            welcome_linear_layout_bluetooth.visibility = View.GONE
-            welcome_linear_layout_programming.visibility = View.VISIBLE
+            showIntroductionProgramming()
         }
         btnNext3.setOnClickListener{
-            welcome_linear_layout_programming.visibility = View.GONE
-            welcome_linear_layout_remote_controller.visibility = View.VISIBLE
+            showIntroductionRemoteController()
         }
         btnNext4.setOnClickListener{
-            welcome_linear_layout_remote_controller.visibility = View.GONE
-            welcome_linear_layout_manage_custom_commands.visibility = View.VISIBLE
+            showIntroductionCustomCommands()
         }
 
         btnSkip1.setOnClickListener{
-            val intent = Intent(this, SelectBluetoothDeviceActivity::class.java)
-            startActivity(intent)
-            finish()
+            exitIntroduction()
         }
         btnSkip2.setOnClickListener{
-            val intent = Intent(this, SelectBluetoothDeviceActivity::class.java)
-            startActivity(intent)
-            finish()
+            exitIntroduction()
         }
         btnSkip3.setOnClickListener{
-            val intent = Intent(this, SelectBluetoothDeviceActivity::class.java)
-            startActivity(intent)
-            finish()
+            exitIntroduction()
         }
         btnSkip4.setOnClickListener{
-            val intent = Intent(this, SelectBluetoothDeviceActivity::class.java)
-            startActivity(intent)
-            finish()
+            exitIntroduction()
         }
 
         btnFinish.setOnClickListener{
-            val intent = Intent(this, SelectBluetoothDeviceActivity::class.java)
-            startActivity(intent)
-            finish()
+            exitIntroduction()
         }
+    }
+
+    private fun isFirstTimeOpeningApp() : Boolean{
+        val preferences = this.getPreferences(Context.MODE_PRIVATE)
+        val isFirstTimeOpeningApp = preferences.getBoolean("IS_FIRST_TIME_OPEN_APP", true)
+
+        with(preferences.edit()){
+            putBoolean("IS_FIRST_TIME_OPEN_APP", false)
+            commit()
+        }
+
+        return isFirstTimeOpeningApp
+    }
+
+    private fun showIntroductionBluetooth(){
+        hideAll()
+        welcome_linear_layout_intro.visibility = View.GONE
+        welcome_linear_layout_bluetooth.visibility = View.VISIBLE
+        introductionState = IntroductionState.INTRODUCTION_BLUETOOTH
+    }
+
+    private fun showIntroductionProgramming(){
+        hideAll()
+        welcome_linear_layout_bluetooth.visibility = View.GONE
+        welcome_linear_layout_programming.visibility = View.VISIBLE
+        introductionState = IntroductionState.INTRODUCTION_PROGRAMMING
+    }
+
+    private fun showIntroductionRemoteController(){
+        hideAll()
+        welcome_linear_layout_programming.visibility = View.GONE
+        welcome_linear_layout_remote_controller.visibility = View.VISIBLE
+        introductionState = IntroductionState.INTRODUCTION_REMOTE_CONTROLLER
+    }
+
+    private fun showIntroductionCustomCommands(){
+        hideAll()
+        welcome_linear_layout_remote_controller.visibility = View.GONE
+        welcome_linear_layout_manage_custom_commands.visibility = View.VISIBLE
+        introductionState = IntroductionState.INTRODUCTION_CUSTOM_COMMANDS
+    }
+
+    private fun exitIntroduction(){
+        hideAll()
+        val intent = Intent(this, SelectBluetoothDeviceActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun hideAll(){
+        welcome_linear_layout_intro.visibility = View.GONE
+        welcome_linear_layout_bluetooth.visibility = View.GONE
+        welcome_linear_layout_programming.visibility = View.GONE
+        welcome_linear_layout_remote_controller.visibility = View.GONE
+        welcome_linear_layout_manage_custom_commands.visibility = View.GONE
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("INTRODUCTION_STATE", introductionState.toString())
     }
 }
